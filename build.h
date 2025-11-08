b4398e0263c0205b839015c6f820f340ff0986a4
Ic3ca0 <166749666+Ic3ca0@users.noreply.github.com>
2025-11-08 11:22:48 -0500

    Create build.h

---
+++ build.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "bplustree.h"
+
+struct PokemonBuild {
+    int         buildId{};
+    int         pokemonId{};
+    std::string name;
+    std::string form;
+    std::string type1;
+    std::string type2;
+
+    int total{};
+    int hp{};
+    int attack{};
+    int defense{};
+    int spAtk{};
+    int spDef{};
+    int speed{};
+    int generation{};
+
+    std::string nature;
+    std::string item;
+
+    int finalHP{};
+    int finalAttack{};
+    int finalDefense{};
+    int finalSpAtk{};
+    int finalSpDef{};
+    int finalSpeed{};
+
+    std::string move1;
+    std::string move2;
+    std::string move3;
+    std::string move4;
+};
+
+class BuildIndex {
+public:
+    void loadFromCSV(const std::string& path);
+
+    std::vector<const PokemonBuild*> findByPokemonId(int pokemonId) const;
+
+private:
+    std::vector<PokemonBuild> builds;
+    BPlusTree<int, std::size_t> byPokemonId;
+};
