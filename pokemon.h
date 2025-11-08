423f4f95f786d65d462d04b094c2e9728c2d4ab6
Ic3ca0 <166749666+Ic3ca0@users.noreply.github.com>
2025-11-08 11:21:34 -0500

    Create pokemon.h

---
+++ pokemon.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "bplustree.h"
+
+struct Pokemon {
+    int         id{};
+    std::string name;
+    std::string form;
+    std::string type1;
+    std::string type2;
+
+    int total{};
+    int hp{};
+    int attack{};
+    int defense{};
+    int spAtk{};
+    int spDef{};
+    int speed{};
+    int generation{};
+};
+
+class PokemonIndex {
+public:
+    void loadFromCSV(const std::string& path);
+
+    const Pokemon* findById(int id) const;
+    std::vector<const Pokemon*> findByName(const std::string& name) const;
+    std::vector<const Pokemon*> findByType(const std::string& type) const;
+
+    std::size_t count() const;
+    bool hasRawId(int id) const;
+
+private:
+    std::vector<Pokemon> data;
+
+    BPlusTree<int, std::size_t>         byId;
+    BPlusTree<std::string, std::size_t> byName;
+    BPlusTree<std::string, std::size_t> byType;
+};
