26a710d0129db9490af0ba8be8d7a70663da3931
Ic3ca0 <166749666+Ic3ca0@users.noreply.github.com>
2025-11-08 15:25:44 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,20 +8,21 @@
 #include "build_index.h"
 #include "pokedex.h"
 #include "printing.h"
+#include "timing.h"
 
-template<typename Func>
-auto timedCall(const std::string& label, Func&& f) -> decltype(f()) {
-    auto start = std::chrono::high_resolution_clock::now();
-    auto result = f();
-    auto end = std::chrono::high_resolution_clock::now();
+// template<typename Func>
+// auto timedCall(const std::string& label, Func&& f) -> decltype(f()) {
+//     auto start = std::chrono::high_resolution_clock::now();
+//     auto result = f();
+//     auto end = std::chrono::high_resolution_clock::now();
 
-    auto us = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-    double ms = us / 1000.0;
+//     auto us = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+//     double ms = us / 1000.0;
 
-    std::cout << "[TIMER] " << label << " took " << ms << " ms\n";
+//     std::cout << "[TIMER] " << label << " took " << ms << " ms\n";
 
-    return result;
-}
+//     return result;
+// }
 
 // int main() {
 //     try {
