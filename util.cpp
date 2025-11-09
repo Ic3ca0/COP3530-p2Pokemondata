4d9075a7f51f3d05258dee402cb92c4ebaa5bb79
Ic3ca0 <166749666+Ic3ca0@users.noreply.github.com>
2025-11-08 22:06:28 -0500

    Update util.cpp

--- util.cpp
+++ util.cpp
@@ -1,6 +1,8 @@
 #include "util.h"
 #include <string>
 
+//inspired by https://docs.python.org/3/library/csv.html
+//https://stackoverflow.com/questions/1120140/how-can-i-read-and-parse-csv-files-in-c
 int parseInt(const std::string& s, int defaultVal) {
     try {
         if (s.empty()) return defaultVal;
